9ba8bc64472813e3b87fcf188a9393b3fbd5ebb8
Asad Khilji <khiljiasad2@gmail.com>
2023-07-07 12:01:00 -0400

    page template

---
+++ assets/js/nav.js
@@ -0,0 +1,22 @@
+const createNav = () => {
+    let nav = document.querySelector('nav');
+
+    nav.innerHTML = `
+    <div class="topnav" id="myTopnav">
+    <nav>
+      <a href="index.html">Islam</a>
+    </nav>
+     
+      <div class="dropdown">
+        <button class="dropbtn">Quran</button>
+        <div class="dropdown-content">
+          <a href="surah-al-fatihah.html">Surah Al Fatihah</a>
+        </div>
+      </div> 
+
+      <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+    </div>
+    `;
+}
+
+createNav();
